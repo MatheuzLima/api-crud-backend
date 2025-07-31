d8542b18b46c72397f1cb3a8e15355943f603d7f
Matheus Lima <contato.matheuslimams@gmail.com>
2025-07-31 09:04:22 -0300

    :broom: Limpeza de comentarios

--- src/database/migrations/20250731025647-create-users.js
+++ src/database/migrations/20250731025647-create-users.js
@@ -35,28 +35,3 @@ export default {
     await queryInterface.dropTable('users');
   }
 }
-
-
-// 'use strict';
-
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   async up (queryInterface, Sequelize) {
-//     /**
-//      * Add altering commands here.
-//      *
-//      * Example:
-//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-//      */
-//   },
-
-//   async down (queryInterface, Sequelize) {
-//     /**
-//      * Add reverting commands here.
-//      *
-//      * Example:
-//      * await queryInterface.dropTable('users');
-//      */
-//   }
-// };
-
